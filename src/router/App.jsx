9fbd41ce4e71fa459db081036ed7bf4bb02cb1c2
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-08-16 19:11:47 +0700

    first commit

---
+++ src/router/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Kelompok 2</h1>
+        </>
+    );
+}
+
+export default App;
