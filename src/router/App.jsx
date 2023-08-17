1b0eb4d0a08ce7ebddffcca8d2257d3a47736e18
Fatah <alifmiftakhulfatah@gmail.com>
2023-08-18 01:26:44 +0700

    slicing ui chat page

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import Hire from "../pages/Home/Hire/index";
+// import Hire from "../pages/Home/Hire/index";
+import ChatPage from "../pages/Chat/index";
 import "../style/style.css";
 
 function App() {
   return (
     <Fragment>
-      <Hire />
+      <ChatPage />
     </Fragment>
   );
 }
