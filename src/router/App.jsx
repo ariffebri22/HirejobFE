0b3cb4b6a4aaa512375d44f4fcad0a4501fd8b6a
Atio <atiowahyudi02@gmail.com>
2023-08-18 14:45:30 +0700

    Edit Profile Workers

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from 'react';
 import Hire from '../pages/Home/Hire/index';
-
+import EditWorker from '../pages/Profile/EditWorkers';
 import '../style/style.css';
 
 function App() {
   return (
     <Fragment>
-      <Hire />
+      <EditWorker/>
+      {/* <Hire /> */}
     </Fragment>
   );
 }
