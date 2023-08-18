9e176cc286c8f821679fc9dc06f1ef6da4266949
Fatah <alifmiftakhulfatah@gmail.com>
2023-08-18 15:02:19 +0700

    slicing ui portfolio

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 // import Hire from "../pages/Home/Hire/index";
-import ChatPage from "../pages/Chat/index";
+// import ChatPage from "../pages/Chat/index";
+import Portfolio from "../pages/Home/Portfolio/index";
 import "../style/style.css";
 
 function App() {
   return (
     <Fragment>
-      <ChatPage />
+      <Portfolio />
     </Fragment>
   );
 }
