183ac9ba3511d040d72b5360437c5e84bc20c8a8
ariffebri22 <muhammadariffebriansyah@gmail.com>
2023-08-18 18:32:02 +0700

    Responsive LandingPage

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <footer className="mt-5 pt-5">
                 <div className="container">
                     <div className="row">
-                        <div className="col-5">
+                        <div className="col-lg-5 col-md-8 col-sm-10">
                             <img src={LogoSecond} alt="Peworld" width="200" />
                             <p className="mt-3 mb-5">Bergabunglah dengan kami di platform pencari kerja kami dan jadilah bagian dari perjalanan menuju karier yang sukses!</p>
                         </div>
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,7 +11,10 @@ const Navbar = () => {
                     <a href="#" className="navbar-brand mt-2">
                         <img src={Logo} alt="Peworld" width="150" />
                     </a>
-                    <div className="d-flex btnNavbar collapse mt-2" id="navbarNav">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="d-flex btnNavbar collapse mt-2" id="navbarSupportedContent">
                         <button className="btn me-4 rounded-1">Masuk Untuk Pekerja</button>
                         <button className="btn rounded-1">Masuk Untuk Perekrut</button>
                     </div>
